de2437a95198ac028cd03303fa7fe35b5210ca5e
unknown <sabian329@gmail.com>
2023-01-24 23:04:20 +0100

    commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
+
+app.post("/", (req, res) => {
+  res.status(200).json({ message: "It worked!" });
+});
+
+app.delete("/", (req, res) => {
+  console.log("Method called is -- ", req.method);
+  res.end();
+});
